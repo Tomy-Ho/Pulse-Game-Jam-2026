19349151371009ed20b7a89969ecab4da47afc91
AntonDo <anton.do003@gmail.com>
2026-08-22 11:06:36 +0200

    Added Player
    Full Movement for a controllable Character

---
+++ PulseGameJam2026/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [NonSerialized] public InputSystem_Actions actions;
+    public float speed = 8f;
+    public float jumpForce = 16f;
+    public Transform groundCheckTransform;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    bool isGrounded;
+    float move;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnEnable()
+    {
+        actions.Player.Enable();
+        actions.Player.Move.performed += Movement;
+        actions.Player.Jump.performed += Jumping;
+
+        actions.Player.Move.canceled += Movement;
+        actions.Player.Jump.canceled += Jumping;
+    }
+
+    void OnDisable()
+    {
+        actions.Player.Disable();
+        actions.Player.Move.performed -= Movement;
+        actions.Player.Jump.performed -= Jumping;
+    }
+
+    void Movement(InputAction.CallbackContext ctx)
+    {
+        move = ctx.ReadValue<Vector2>().x;
+    }
+
+    void Jumping(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed & isGrounded)
+        {
+            rb.linearVelocityY = jumpForce;
+        }
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundLayer);
+        rb.linearVelocityX = move * speed;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheckTransform.position, groundCheckRadius);
+    }
+}
