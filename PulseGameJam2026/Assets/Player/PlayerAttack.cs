5a70406c9d05ec3208dd6d570f91b33dee5fd22c
tonqi <n.thanglong32@gmail.com>
2026-08-23 00:31:32 +0200

    Player Attack update, Meditate update
    Attack: Now you lose hp with each attack
    Meditate: Regain hp when meditating

--- PulseGameJam2026/Assets/Player/PlayerAttack.cs
+++ PulseGameJam2026/Assets/Player/PlayerAttack.cs
@@ -12,13 +12,17 @@ public class PlayerAttack : MonoBehaviour
     public int attackDamage = 25;
     public float cooldownTime = 0.5f;
     float cooldownTimer = 0f;
-
+    private HealthManager playerHealth;
+    public GameObject player;
+    public bool canAttack = true;
+  
     [NonSerialized] public InputSystem_Actions actions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         actions = new InputSystem_Actions();
         actions.Player.Attack.AddBinding("<Mouse>/leftButton");
+        
     }
 
     void OnEnable()
@@ -34,24 +38,29 @@ public class PlayerAttack : MonoBehaviour
     }
     void Start()
     {
-        
+        playerHealth = player.GetComponent<HealthManager>();
     }
 
     // Update is called once per frame
     void OnAttack(InputAction.CallbackContext ctx)
     {
-        if(cooldownTimer <= 0)
-        {
-            Collider2D[] enemy = Physics2D.OverlapCircleAll(attackOrigin.position, attackRadius, enemyMask);
-            foreach (var enemies in enemy)
+        if(canAttack){
+            playerHealth.currentHealth -= 3;
+            playerHealth.healthBar.SetCurrentHealth(playerHealth.currentHealth);
+            if(cooldownTimer <= 0)
             {
-                enemies.GetComponent<HealthManager>().TakeDamage(attackDamage);
+                Collider2D[] enemy = Physics2D.OverlapCircleAll(attackOrigin.position, attackRadius, enemyMask);
+                foreach (var enemies in enemy)
+                {
+                    enemies.GetComponent<HealthManager>().TakeDamage(attackDamage);
+                }
+                cooldownTimer = cooldownTime;
+                
             }
-            cooldownTimer = cooldownTime;
-        }
-        else
-        {
-            cooldownTimer -= Time.deltaTime;
+            else
+            {
+                cooldownTimer -= Time.deltaTime;
+            }   
         }
     }
 
--- PulseGameJam2026/Assets/Scripts/MeditateAbility.cs
+++ PulseGameJam2026/Assets/Scripts/MeditateAbility.cs
@@ -6,10 +6,14 @@ public class MeditateAbility : Ability
     private int counter;
     private float normalSpeed;
     private PlayerMovement playerMovement;
+    private HealthManager playerHealth;
+    private PlayerAttack playerAttack;
+    public int healthRegen;
     public override void Activate(GameObject parent) {
         // Meditate can be used anytime, hold to meditate, fill "sanity" bar, lock player movement, play meditate animation
         playerMovement = parent.GetComponent<PlayerMovement>();
-        normalSpeed = playerMovement.speed;
+        playerAttack = parent.GetComponent<PlayerAttack>();
+        normalSpeed = 4;    
 
         SpriteRenderer sr = parent.GetComponent<SpriteRenderer>(); // FOR TESTING: CHANGE COLOR OF CIRCLE
         if(sr != null) sr.color = Color.blue;
@@ -17,10 +21,15 @@ public class MeditateAbility : Ability
     }
 
     public override void AbilityLoop(GameObject parent) {
-        PlayerMovement playerMovement = parent.GetComponent<PlayerMovement>();
         playerMovement.speed = 0;
         playerMovement.canJump = false;
+        playerAttack.canAttack = false;
         //TODO: INCREASE SANITY + SANITY BAR
+        playerHealth = parent.GetComponent<HealthManager>();
+        if(playerHealth.currentHealth <= playerHealth.maxHealth - healthRegen){
+            playerHealth.currentHealth += healthRegen;  
+            playerHealth.healthBar.SetCurrentHealth(playerHealth.currentHealth);    
+        }   
 
         // counter++; //FOR TESTING
         // Debug.Log("Counter: " + counter);
@@ -28,9 +37,9 @@ public class MeditateAbility : Ability
 
 
     public override void BeginCooldown(GameObject parent) {
-        PlayerMovement playerMovement = parent.GetComponent<PlayerMovement>();
         playerMovement.speed = normalSpeed;
         playerMovement.canJump = true;
+        playerAttack.canAttack = true;
         
         SpriteRenderer sr = parent.GetComponent<SpriteRenderer>();
         if (sr != null) sr.color = Color.white;
