82db4f78c3ec2791fa4c7fce070dfd1b521d18a7
Tomy-Ho <ho.duong.tomy@gmail.com>
2026-08-22 19:57:54 +0200

    Reapply "Merge branch 'dev' of https://github.com/Tomy-Ho/Pulse-Game-Jam-2026 into dev"
    This reverts commit 28dc10aa18966a55b8ef1e701dc912da5b821042.

---
+++ PulseGameJam2026/Assets/Scripts/AbilityHolderPRESS.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AbilityHolderPRESS : MonoBehaviour
+{
+    public GameObject player;
+    public Ability ability;
+    float cooldownTime;
+    float activeTime;
+    enum AbilityState
+    {
+        ready,
+        active,
+        cooldown
+    }
+
+    AbilityState state = AbilityState.ready;
+    
+    public Key key;
+
+    void Update(){
+        switch(state) {
+            case AbilityState.ready:
+                if (Keyboard.current[key].wasPressedThisFrame){
+                    ability.Activate(gameObject);
+                    state = AbilityState.active;
+                    activeTime = ability.activeTime;
+                }
+                break;
+            case AbilityState.active:
+                if(activeTime > 0){
+                    activeTime -= Time.deltaTime;
+                }
+                else {
+                    ability.BeginCooldown(gameObject);
+                    state = AbilityState.cooldown;
+                    cooldownTime = ability.cooldownTime;                    
+                }
+
+                break;
+            case AbilityState.cooldown:
+                if(cooldownTime > 0) {
+                    cooldownTime -= Time.deltaTime;
+                }
+                else {
+                    state = AbilityState.ready;
+                }
+                break;
+        }
+    }
+}
